53b69de4b6740438be57f51afb7b6b1bb59799a0
VXT <VXT>
2024-07-29 20:43:53 +0700

    Second Commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,72 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+test.describe("Home Page Test", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  test("should have elements", async ({ page }) => {
+    await expect(page).toHaveTitle("Demo Playwright");
+
+    await expect(
+      page.getByRole("heading", {
+        name: "Home Page",
+      })
+    ).toBeVisible();
+
+    await expect(
+      page.getByRole("link", {
+        name: "Form",
+      })
+    ).toBeVisible();
+  });
+
+  test("should redirect to Form Page when click", async ({ page }) => {
+    await page.getByRole("link", { name: "Form" }).click();
+
+    await expect(page).toHaveTitle("Form");
+  });
 });
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+test.describe("Form Page Test", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:3000/form");
+  });
 
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
+  test("should have elements", async ({ page }) => {
+    await expect(page).toHaveTitle("Form");
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Installation" })
-  ).toBeVisible();
+    await expect(
+      page.getByRole("heading", {
+        name: "Form",
+      })
+    ).toBeVisible();
+
+    await expect(page.getByPlaceholder("Enter item")).toBeVisible();
+
+    await expect(
+      page.getByRole("button", {
+        name: "Add",
+      })
+    ).toBeVisible();
+  });
+
+  test("should have empty items list on start", async ({ page }) => {
+    const itemsList = page.getByTestId("items-list");
+
+    await expect(itemsList).toBeEmpty();
+  });
+
+  test("should add item to list", async ({ page }) => {
+    const input = page.getByPlaceholder("Enter Item");
+
+    await input.fill("Item 1");
+
+    await page.getByRole("button", { name: "Add" }).click();
+
+    const item = page.getByTestId("item").nth(0);
+
+    await expect(item).toHaveText("Item 1");
+    await expect(input).toBeEmpty();
+  });
 });
